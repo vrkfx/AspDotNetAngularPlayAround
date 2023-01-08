0341917c4d5146f481e92bcd5e2972fa23e455f3
Virroshan_Kumar <virroshan08@outlook.com>
2023-01-09 07:13:03 +1030

    Created new project from scratch with the database with only one table so far but got 1 to work

---
+++ BookApi/BookApi/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Data;
+using BookApi.Models;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        private readonly BookDBContext _context;
+
+        public AuthorsController(BookDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Author
+        //[HttpGet]
+        //public async Task<IActionResult> Index()
+        //{
+        //      return View(await _context.Authors.ToListAsync());
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+    }
+}
---
+++ BookApi/BookApi/Program.cs
@@ -0,0 +1,41 @@
+
+using BookApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add BookDBContext.cs
+
+builder.Services.AddDbContext<BookApi.Data.BookDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookDB"));
+});
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
